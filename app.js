61cf8dfc7694fabc841b1e2ceb5d3af2c0bb4263
BullsI808 <JBasuil8697@myhta.org>
2018-04-19 15:05:33 -1000

    finished the main code

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-console.log('heck you');
-window.onload
+window.onload = function() {
+    var form = document.getElementById("Form");
+    var petName = document.getElementById("petName");
+    var petType = document.getElementById("petType");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var description = document.getElementById("description");
+    var legs = document.getElementsByClassName("legs");
+    var special = document.getElementsByClassName('special');
+    var obj = {};
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        obj.name = petName.value;
+        obj.petType = petType.value;
+        obj.email = email.value;
+        obj.password = password.value;
+        obj.description = description.value;
+        for(let i = 0 ; i < legs.length ; i++){
+            if (legs[i].checked === true){
+                obj.legs = legs[i].value;
+            }
+        }
+        for(let i = 0 ; i < special.length ; i++){
+            if (special[i].checked === true){
+                obj.special = special[i].value;
+            }
+        }
+
+        console.log(obj);
+    });
+};
